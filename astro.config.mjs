c94d65eaa3751e275ab81bf53fc689fa05f314c4
Yoyo Janeiro <johannapalminha@gmail.com>
2024-03-03 18:04:38 +0100

    Initial commi

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://SStyles93.github.io',
+  site: 'https://YoyoJaneiro.github.io',
   integrations: [mdx(), sitemap(), tailwind()]
 });
--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Samuel Styles | Personal Portfolio Website';
-export const SITE_DESCRIPTION = 'Website designed to contain all my work, projects and blogs';
+export const SITE_TITLE = 'Yoyo Janeiro | Personal Portfolio Website';
+export const SITE_DESCRIPTION = 'Website designed to contain all my work';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
